91e08eeee42367a71b4eb0c5898cfa76f9cffc30
daddyjanno <jn.drugmand@gmail.com>
2024-10-24 11:06:12 +0200

    initialize Pixi app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import { Application } from 'pixi.js'
+
 console.log('Generative code')
+
+const app = new Application()
+await app.init({
+    resizeTo: window,
+    // width: 400,
+    // height: 300,
+    antialias: true,
+})
+document.body.appendChild(app.canvas)
