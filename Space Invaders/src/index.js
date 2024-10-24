5fcd76927af802a9081ad9085d453c59e40fe270
daddyjanno <jn.drugmand@gmail.com>
2024-10-24 12:07:14 +0200

    randomize matrix

--- Space Invaders/src/index.js
+++ Space Invaders/src/index.js
@@ -15,12 +15,20 @@ const matrix = []
 const spaceInvaderSize = 7
 
 function generateInvader() {
+    // generate base matrix
     for (let i = 0; i < spaceInvaderSize; i++) {
         const row = []
         for (let j = 0; j < spaceInvaderSize; j++) {
             row.push(0)
         }
         matrix.push(row)
+    }
+
+    //randomize values for the first 4 columns
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < Math.ceil(matrix[i].length / 2); j++) {
+            matrix[i][j] = Math.round(Math.random())
+        }
     }
 }
 generateInvader()
