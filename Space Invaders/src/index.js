0ce806cb002f054c6142b647d513d4b785b8c2d7
daddyjanno <jn.drugmand@gmail.com>
2024-10-24 12:01:17 +0200

    create matrix loop

--- Space Invaders/src/index.js
+++ Space Invaders/src/index.js
@@ -10,3 +10,18 @@ await app.init({
     antialias: true,
 })
 document.body.appendChild(app.canvas)
+
+const matrix = []
+const spaceInvaderSize = 7
+
+function generateInvader() {
+    for (let i = 0; i < spaceInvaderSize; i++) {
+        const row = []
+        for (let j = 0; j < spaceInvaderSize; j++) {
+            row.push(0)
+        }
+        matrix.push(row)
+    }
+}
+generateInvader()
+console.log(matrix)
