e6643522b037fe8913ce12e12ecb6f8528340507
daddyjanno <jn.drugmand@gmail.com>
2024-10-25 14:18:07 +0200

    index

--- Space Invaders/src/index.js
+++ Space Invaders/src/index.js
@@ -13,25 +13,46 @@ globalThis.__PIXI_APP__ = app
 document.body.appendChild(app.canvas)
 
 const spaceInvaderSize = 14
-const pixelSize = 3
-const color = 'white'
+const pixelSize = 10
+const color = Math.floor(Math.random() * 0xffffff)
+
 const gap = pixelSize * 2
+const invaderSize = pixelSize * spaceInvaderSize
 
-const invaderMatrix = generateInvaderMatrix(spaceInvaderSize)
-
+const matrix = generateInvaderMatrix(spaceInvaderSize)
 const matrixContainer = new Container()
+const invader = drawInvader(
+    matrix,
+    color,
+    pixelSize,
+    pixelSize,
+    spaceInvaderSize
+)
+matrixContainer.addChild(invader)
 
-for (let i = 0; i < 100; i++) {
-    let invader = drawInvader(invaderMatrix, 'white', pixelSize, pixelSize)
+// for (let i = 0; i < 100; i++) {
+//     const invaderMatrix = generateInvaderMatrix(spaceInvaderSize)
+//     let invader = drawInvader(
+//         invaderMatrix,
+//         color,
+//         pixelSize,
+//         pixelSize,
+//         spaceInvaderSize
+//     )
+//     console.log('invaderSize : ', invader.width, invader.height)
 
-    invader.x = invader.width * (i % 10) + (i % 10) * gap
-    let tens = Math.floor(i / 10)
-    console.log(tens)
+//     let tens = Math.floor(i / 10)
+//     const row = Math.floor(i / 10) + 1
+//     const col = i % 10
 
-    invader.y = invader.height * tens + tens * gap
+//     invader.x = invaderSize * col + col * gap
+//     invader.y = invaderSize * row + row * gap
 
-    matrixContainer.addChild(invader)
-}
+//     console.log('i :', i, 'tens: ', tens, 'row :', row, 'col :', col)
+//     console.log('x: ', invader.x, 'y :', invader.y)
+
+//     matrixContainer.addChild(invader)
+// }
 matrixContainer.x = app.screen.width / 2 - matrixContainer.width / 2
 matrixContainer.y = app.screen.height / 2 - matrixContainer.height / 2
 
