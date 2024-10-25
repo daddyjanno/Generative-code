77a54144d59913b9355262b748fd7147f3b49f7a
daddyjanno <jn.drugmand@gmail.com>
2024-10-25 10:58:14 +0200

    import new function

--- Space Invaders/src/index.js
+++ Space Invaders/src/index.js
@@ -1,4 +1,5 @@
 import { Application, Container, Graphics } from 'pixi.js'
+import { generateInvader } from './generateInvaderMatrix'
 
 console.log('Generative code')
 
@@ -10,40 +11,10 @@ await app.init({
 globalThis.__PIXI_APP__ = app
 document.body.appendChild(app.canvas)
 
-const matrix = []
 const spaceInvaderSize = 7
 const pixelSize = 32
 
-function generateInvader() {
-    // generate base matrix
-    for (let i = 0; i < spaceInvaderSize; i++) {
-        const row = []
-        for (let j = 0; j < spaceInvaderSize; j++) {
-            row.push({ value: 0, alpha: 0 })
-        }
-        matrix.push(row)
-    }
-
-    //randomize values for the first 4 columns
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < Math.ceil(matrix[i].length / 2); j++) {
-            matrix[i][j].value = Math.round(Math.random())
-            matrix[i][j].alpha = 0.25 + Math.random() * 0.75
-        }
-    }
-
-    // Symetry
-    for (let i = 0; i < matrix.length; i++) {
-        for (let j = 0; j < Math.floor(matrix[i].length / 2); j++) {
-            const valueToDuplicate = matrix[i][j]
-
-            matrix[i][matrix.length - 1 - j] = valueToDuplicate
-        }
-    }
-}
-console.log(matrix)
-
-generateInvader()
+const invaderMatrix = generateInvader(spaceInvaderSize)
 
 function createPixel() {
     const p = new Graphics().rect(0, 0, pixelSize, pixelSize).fill(0xffffff)
@@ -53,14 +24,14 @@ function createPixel() {
 function drawInvader() {
     const invaderContainer = new Container()
 
-    for (let i = 0; i < matrix.length; i++) {
-        const row = matrix[i]
+    for (let i = 0; i < invaderMatrix.length; i++) {
+        const row = invaderMatrix[i]
         for (let j = 0; j < row.length; j++) {
-            if (matrix[i][j].value) {
+            if (invaderMatrix[i][j].value) {
                 const newPixel = createPixel()
                 newPixel.y = i * pixelSize
                 newPixel.x = j * pixelSize
-                newPixel.alpha = matrix[i][j].alpha
+                newPixel.alpha = invaderMatrix[i][j].alpha
                 invaderContainer.addChild(newPixel)
             }
         }
