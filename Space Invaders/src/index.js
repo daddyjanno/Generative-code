ddece29bdd0b8a0165df9595b63e8cd3d1167f37
daddyjanno <jn.drugmand@gmail.com>
2024-10-25 11:12:06 +0200

    refacto index.js

--- Space Invaders/src/index.js
+++ Space Invaders/src/index.js
@@ -1,5 +1,7 @@
 import { Application, Container, Graphics } from 'pixi.js'
 import { generateInvader } from './generateInvaderMatrix'
+import { generatePixel } from './generatePixel'
+import { drawInvader } from './drawInvader'
 
 console.log('Generative code')
 
@@ -12,37 +14,9 @@ globalThis.__PIXI_APP__ = app
 document.body.appendChild(app.canvas)
 
 const spaceInvaderSize = 7
-const pixelSize = 7
+const pixelSize = 14
+const color = Math.random() * 0xffffff
 
 const invaderMatrix = generateInvader(spaceInvaderSize)
 
-const color = Math.random() * 0xffffff
-
-function createPixel(color) {
-    const pixelContainer = new Container()
-    const p = new Graphics().rect(0, 0, pixelSize, pixelSize).fill(color)
-    pixelContainer.addChild(p)
-    return pixelContainer
-}
-
-function drawInvader() {
-    const invaderContainer = new Container()
-
-    for (let i = 0; i < invaderMatrix.length; i++) {
-        const row = invaderMatrix[i]
-        for (let j = 0; j < row.length; j++) {
-            if (invaderMatrix[i][j].value) {
-                const newPixel = createPixel(color)
-                newPixel.y = i * pixelSize
-                newPixel.x = j * pixelSize
-                newPixel.alpha = invaderMatrix[i][j].alpha
-                invaderContainer.addChild(newPixel)
-            }
-        }
-    }
-    invaderContainer.x = app.screen.width / 2 - invaderContainer.width / 2
-    invaderContainer.y = app.screen.height / 2 - invaderContainer.height / 2
-    console.log(invaderContainer.width, invaderContainer.height)
-    app.stage.addChild(invaderContainer)
-}
-drawInvader()
+const invader = drawInvader(app, invaderMatrix, color, pixelSize, pixelSize)
